2aafefc7c0bf41df7e5512b9caef35443dbcf956
DevbyZed <capella@chzirbes.be>
2016-07-13 16:27:24 +0200

    adapt iPhone

--- nl/boeking/inc/header.php
+++ nl/boeking/inc/header.php
@@ -43,7 +43,7 @@
         <div class="container">
             <div class="navbar-header">
                 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
-                    <i class="fa fa-bars"></i>
+                    <span class="glyphicon glyphicon-option-vertical" aria-hidden="true"></span>
                 </button>
                 <a class="navbar-brand page-scroll" href="index.php">
                     <i class="fa fa-play-circle"></i>  Countrytickets.eu - Reservering
