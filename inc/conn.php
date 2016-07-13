8ddca83fa3d57d3d69cc0bc5cbe509fb2cd24b95
ZIRBES Christian <ZIRBES Christian>
2016-07-13 09:06:17 +0200

    add all pdo

--- inc/conn.php
+++ inc/conn.php
@@ -1,4 +1,9 @@
 <?php
-$pdo = new PDO('mysql:dbname=hb2016;host=localhost', 'root', 'root');
+//$pdo = new PDO('mysql:dbname=hb2016;host=localhost', 'root', 'root'); // Mac
+$pdo = new PDO('mysql:dbname=hb2016;host=localhost', 'root', 'admin');  // Win 10
+//$pdo = new PDO('mysql:dbname=countrytickets_eu_hand2016;host=countrytickets.eu.mysql', 'countrytickets_eu_hand2016', 'handadmin'); // one.com
+
+
+
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
