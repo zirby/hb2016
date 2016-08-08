ea7f9f3b443ea3cacdaae89d7920c8ccc130e8d0
DevbyZed <capella@chzirbes.be>
2016-08-08 09:11:04 +0200

    register correction

--- fr/reservation/logout.php
+++ fr/reservation/logout.php
@@ -2,7 +2,7 @@
 
 session_start();
 //$j = substr($_SESSION['jour'], 3);
-$index = "index".$j.".php";
+$index = "index.php";
 unset($_SESSION['auth']);
 setcookie('remember', NULL, -1);
 $_SESSION['flash']['success'] = "Vous êtes maintenant déconnecté";
